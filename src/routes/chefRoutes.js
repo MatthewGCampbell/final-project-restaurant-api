2a2cd47550b68042369dafad6e0ff4e3d10d8583
Eric Nguyen <mnguye73@charlotte.edu>
2025-11-18 13:17:34 -0500

    stash

--- src/routes/chefRoutes.js
+++ src/routes/chefRoutes.js
@@ -2,14 +2,15 @@ import express from 'express'
 import { getAllChefsHandler, addChefHandler, updateChefHandler, deleteChefHandler, getChefByIdHandler } from '../controllers/chefController.js'
 import { validateChefQuery, validateChefBody, validateChefId } from '../middleware/chefValidators.js'
 import { authenticate } from '../middleware/authenticate.js'
+import { authorizeRoles } from '../middleware/authorizeRoles.js'
 
 // need to add validators and authenticate/authororization
 
 const router = express.Router();
 router.get('/', validateChefQuery, getAllChefsHandler);
 router.get('/:id', validateChefId, getChefByIdHandler);
-router.post('/', authenticate, validateChefBody, addChefHandler);
+router.post('/', authenticate, authorizeRoles('HEAD_CHEF'), validateChefBody, addChefHandler);
 router.put('/:id', validateChefId, validateChefBody, updateChefHandler);
-router.delete('/:id', validateChefId, deleteChefHandler);
+router.delete('/:id', validateChefId, authorizeRoles('HEAD_CHEF'), deleteChefHandler);
 
 export default router; 
